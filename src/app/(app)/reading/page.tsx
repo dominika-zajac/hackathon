5bcbb1abd906951f6184d3575cdb1b1a043ac977
Виктор Мороз <octopuzik10@gmail.com>
2025-10-19 10:47:23 +0000

    margin-left: 30px; (_for element div_)

--- src/app/(app)/reading/page.tsx
+++ src/app/(app)/reading/page.tsx
@@ -2,7 +2,7 @@ import ReadingClient from './client';
 
 export default function ReadingPage() {
   return (
-    <div className="flex-1 space-y-8 p-4 pt-6 md:p-8">
+    <div className="ml-[30px] flex-1 space-y-8 p-4 pt-6 md:p-8">
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight font-headline">Reading Suggestions</h2>
       </div>
